4811959ff7b2931e58078a0ae519b4342f8d44a1
harshild <harshil.dhariwal@gmail.com>
2020-10-26 13:02:37 -0400

    Changes for multi select dropdown

--- src/app/shared/navbar/navbar.module.ts
+++ src/app/shared/navbar/navbar.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
-    imports: [ RouterModule, CommonModule, NgbModule ],
+  imports: [RouterModule, CommonModule, NgbModule, NgSelectModule, FormsModule],
     declarations: [ NavbarComponent ],
-    exports: [ NavbarComponent ]
+    exports: [ NavbarComponent ],
 })
 
 export class NavbarModule {}
