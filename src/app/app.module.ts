2c829f1c223558429450824e5c0dc0562b5636fc
harshild <harshil.dhariwal@gmail.com>
2020-10-26 21:32:53 -0400

    Added login component Added helpers that came with the source using https://jasonwatmore.com/post/2020/07/18/angular-10-user-registration-and-login-example-tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { ToastrModule } from "ngx-toastr";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {ToastrModule} from 'ngx-toastr';
 
-import { SidebarModule } from './sidebar/sidebar.module';
-import { FooterModule } from './shared/footer/footer.module';
-import { NavbarModule} from './shared/navbar/navbar.module';
-import { FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
+import {SidebarModule} from './sidebar/sidebar.module';
+import {FooterModule} from './shared/footer/footer.module';
+import {NavbarModule} from './shared/navbar/navbar.module';
+import {FixedPluginModule} from './shared/fixedplugin/fixedplugin.module';
 
-import { AppComponent } from './app.component';
-import { AppRoutes } from './app.routing';
+import {AppComponent} from './app.component';
+import {AppRoutes} from './app.routing';
 
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { LoginComponent } from './login/login.component';
+import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ErrorInterceptor, fakeBackendProvider, JwtInterceptor} from './_helpers';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    LoginComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(AppRoutes,{
       useHash: true
@@ -32,7 +33,13 @@ import { LoginComponent } from './login/login.component';
     FooterModule,
     FixedPluginModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,13 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
-import { LoginComponent } from '../app/login/login.component'
+import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.component';
+
+import {AuthGuard} from './_helpers';
+
+const accountModule = () => import('./account/account.module').then(x => x.AccountModule);
+const adminModule = () => import('./layouts/admin-layout/admin-layout.module').then(x => x.AdminLayoutModule);
+const usersModule = () => import('./users/users.module').then(x => x.UsersModule);
+
 export const AppRoutes: Routes = [
   {
     path: '',
@@ -9,17 +15,19 @@ export const AppRoutes: Routes = [
     pathMatch: 'full',
   },
   {
-    path: 'login',
-    component: LoginComponent
-  }, 
-  {
     path: '',
     component: AdminLayoutComponent,
     children: [
-        {
-      path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-  }]},
+      {
+        path: '',
+        loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+      }],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'account',
+    loadChildren: accountModule
+  },
   {
     path: '**',
     redirectTo: 'dashboard'
