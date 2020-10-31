06c12d711616fe8f147a5013f26fedab109bbf59
harshild <harshil.dhariwal@gmail.com>
2020-10-31 14:26:57 -0400

    Removed unused code from navbar

--- src/app/shared/navbar/navbar.module.ts
+++ src/app/shared/navbar/navbar.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgSelectModule } from '@ng-select/ng-select';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {NavbarComponent} from './navbar.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgSelectModule} from '@ng-select/ng-select';
 import {FormsModule} from '@angular/forms';
 
 @NgModule({
