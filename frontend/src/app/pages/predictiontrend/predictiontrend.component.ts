d13982a60aac53b958fdbf178352367419b4bfe5
harshild <harshil.dhariwal@gmail.com>
2020-11-23 21:28:06 -0500

    Refactoring

--- frontend/src/app/pages/predictiontrend/predictiontrend.component.ts
+++ frontend/src/app/pages/predictiontrend/predictiontrend.component.ts
@@ -82,28 +82,6 @@ export class PredictionTrendComponent implements OnInit {
 
       const speedCanvas = document.getElementById('speedChart');
 
-      const dataFirst = {
-        data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],
-        fill: false,
-        borderColor: '#fbc658',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#fbc658',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8,
-      };
-
-      const dataSecond = {
-        data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],
-        fill: false,
-        borderColor: '#51CACF',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#51CACF',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8
-      };
-
       console.log(chartData)
 
       const speedData = {
