7cba863f4f8c0f84ab863db4ffcb62a02c30aa18
harshild <harshil.dhariwal@gmail.com>
2020-12-02 23:09:35 -0500

    Reorganized sidebar for clarity

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -1,29 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 
 export interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  id: string
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+  child: RouteInfo[]
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard',     title: 'Dashboard',         icon: 'nc-bank',       class: '' },
-    { path: '/trend',     title: 'Current Trend',         icon: 'nc-chart-bar-32',       class: '' },
-    { path: '/prediction-trend',     title: 'Demand Forecast',         icon: 'nc-atom',       class: '' },
-    { path: '/comparison',     title: 'Comparison',         icon: 'nc-chart-pie-36',       class: '' },
+  {id: '', path: '/dashboard', title: 'Home', icon: 'nc-bank', class: '', child: null},
+  {id: 'current', path: '/nopath', title: 'Current Trend', icon: 'nc-chart-bar-32', class: '', child: [
+      {id: '', path: '/trend', title: 'Overview', icon: 'nc-chart-bar-32', class: 'pl-5', child: null},
+      {id: '', path: '/comparison', title: 'Compare Technologies', icon: 'nc-chart-pie-36', class: 'pl-5', child: null},
+    ]
+  },
+  {id: '', path: '/prediction-trend', title: 'Demand Forecast', icon: 'nc-atom', class: '', child: null},
 ];
 
 @Component({
-    moduleId: module.id,
-    selector: 'app-sidebar-cmp',
-    templateUrl: 'sidebar.component.html',
+  moduleId: module.id,
+  selector: 'app-sidebar-cmp',
+  templateUrl: 'sidebar.component.html',
 })
 
 export class SidebarComponent implements OnInit {
-    public menuItems: any[];
-    ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
-    }
+  public menuItems: any[];
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
 }
