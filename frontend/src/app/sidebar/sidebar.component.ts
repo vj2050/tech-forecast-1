1a5f53ecf74e34b29253435ed64c56e2d7328149
harshild <harshil.dhariwal@gmail.com>
2020-12-03 01:48:32 -0500

    Changed the home page Change sidebar items

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -11,13 +11,13 @@ export interface RouteInfo {
 }
 
 export const ROUTES: RouteInfo[] = [
-  {id: '', path: '/dashboard', title: 'Home', icon: 'nc-bank', class: '', child: null},
+  {id: '', path: '/dashboard', title: 'About', icon: 'nc-bank', class: '', child: null},
   {id: 'current', path: '/nopath', title: 'Current Trend', icon: 'nc-chart-bar-32', class: '', child: [
       {id: '', path: '/trend', title: 'Overview', icon: 'nc-chart-bar-32', class: 'pl-5', child: null},
-      {id: '', path: '/comparison', title: 'Compare Technologies', icon: 'nc-chart-pie-36', class: 'pl-5', child: null},
+      {id: '', path: '/comparison', title: 'Compare', icon: 'nc-chart-pie-36', class: 'pl-5', child: null},
     ]
   },
-  {id: '', path: '/prediction-trend', title: 'Demand Forecast', icon: 'nc-atom', class: '', child: null},
+  {id: '', path: '/prediction-trend', title: 'Popularity Forecast', icon: 'nc-atom', class: '', child: null},
 ];
 
 @Component({
